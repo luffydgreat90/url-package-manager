7a7dfd99f1e1a6c7fd902d20e6631be272903f91
Marlon Ansale <117570623+luffydgreat90@users.noreply.github.com>
2024-05-09 00:27:11 +0100

    update

--- Package.swift
+++ Package.swift
@@ -10,15 +10,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "APIPackage",
-            targets: ["AnyIOSCore/Sources/APIPackage"]),
+            targets: ["APIPackage"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "APIPackage"),
-        .testTarget(
-            name: "APIPackageTests",
-            dependencies: ["AnyIOSCore/Tests/APIPackageTests"]),
+            name: "APIPackage")
     ]
 )
